f0a972d656da1eff6a4b2bf5f0070a89586f0e22
Robert Sösemann <robert@soesemann.com>
2025-02-09 15:24:54 +0100

    Copilot refactored  explainer

--- force-app/main/default/lwc/singleChatMessage/singleChatMessage.js
+++ force-app/main/default/lwc/singleChatMessage/singleChatMessage.js
@@ -1,8 +1,6 @@
 import { LightningElement, api } from 'lwc';
-
 import steps from '@salesforce/apex/ChatCtrl.steps';
 import modify from '@salesforce/apex/ChatCtrl.modify';
-
 import butlerLogo from '@salesforce/contentAssetUrl/myorgbutlertransparent_720';
 
 export const MESSAGE_TYPE = {
@@ -15,9 +13,8 @@ export default class SingleChatMessage extends LightningElement {
     @api type = MESSAGE_TYPE.OUTBOUND;
 
     butlerLogo = butlerLogo;
-    explanation;
-    explanationInitialized = false;
-    visibleQuestion = true;
+    explanationSteps = [];
+    isExpanded = false;
 
     get isButlerLogoVisible() {
         return this.type === MESSAGE_TYPE.INBOUND;
@@ -25,10 +22,6 @@ export default class SingleChatMessage extends LightningElement {
 
     get isExplanationVisible() {
         return this.type === MESSAGE_TYPE.INBOUND;
-    }
-
-    get visibleMessage() {
-        return this.visibleQuestion ? this.message.content : this.explanation;
     }
 
     get listItemClass() {
@@ -43,56 +36,40 @@ export default class SingleChatMessage extends LightningElement {
         return this.type === MESSAGE_TYPE.INBOUND;
     }
 
-    getCodeBlockStyling(code) {
-        return code ? '<div style="background-color: rgb(250, 250, 250); border-radius: 10px; padding:10px"><code>'  + code + '</code></div>' : '';
+    get toggleIcon() {
+        return this.isExpanded ? 'utility:chevrondown' : 'utility:chevronright';
+    }
+
+    get contentClass() {
+        return `slds-p-left_small explanation-content ${this.isExpanded ? 'expanded' : ''}`;
+    }
+
+    async connectedCallback() {
+        if (this.type === MESSAGE_TYPE.INBOUND) {
+            await this.getExplanation();
+        }
     }
 
     async getExplanation() {
         const stepResult = await steps({runId : this.message.runId, order: 'asc'});
 
-        let toolCallouts = [];
-        for (let step = 0; step < stepResult.data.length; step++) {
-            const data =  stepResult.data[step];
-
-            if(data.type === 'tool_calls') {
-                for (let i = 0; i < data.step_details.tool_calls.length; i++) {
-                    const toolCall = data.step_details.tool_calls[i];
-                    if(toolCall.type === 'function'){
-                        const toolCallArgs = JSON.parse(toolCall.function.arguments);
-
-                        if (toolCall.function.name === 'Call_Salesforce_API') {
-                            const method = toolCallArgs.httpMethod;
-                            const uri = toolCallArgs.urlIncludingParams;
-                            const uriMethod = this.getCodeBlockStyling(method + ' ' + uri);
-
-                            const body = toolCallArgs.body ? this.getCodeBlockStyling(toolCallArgs.body) : '';
-                            const output = toolCall.function.output ? '\n\tOutput' + this.getCodeBlockStyling(toolCall.function.output) : '';
-
-                            const stepInfo = (toolCallouts.length + 1) + '. ' + uriMethod + body + output;
-                            toolCallouts.push(stepInfo);
-                        } else if (toolCall.function.name === 'Notify_User') {
-                            const title = '\n\tTitle' + this.getCodeBlockStyling(toolCallArgs.title);
-                            const body = '\n\tBody' + this.getCodeBlockStyling(toolCallArgs.body);
-                            const output = toolCall.function.output ? '\n\tOutput' + this.getCodeBlockStyling(toolCall.function.output) : '';
-
-                            const stepInfo = (toolCallouts.length + 1) + '. Notification\n' + title + body + output;
-                            toolCallouts.push(stepInfo);
-                        }
-                    }
-                }
-            }
-        }
-
-        this.explanation = '`Steps:` \n' + toolCallouts.join('\n');
-        this.explanationInitialized = true;
+        this.explanationSteps = stepResult.data
+            .filter(data => data.type === 'tool_calls')
+            .flatMap(data => data.step_details.tool_calls)
+            .filter(toolCall => toolCall.type === 'function')
+            .map((toolCall, index) => {
+                const args = JSON.parse(toolCall.function.arguments);
+                return {
+                    id: `step-${index}`,
+                    title: `${index + 1}. ${toolCall.function.name.replace(/_/g, ' ')}`,
+                    args: JSON.stringify(args, null, 2),
+                    output: toolCall.function.output
+                };
+            });
     }
 
-    async handleExplain() {
-        if(!this.explanationInitialized) {
-            await this.getExplanation();
-        }
-
-        this.visibleQuestion = !this.visibleQuestion;
+    toggleExplanation() {
+        this.isExpanded = !this.isExpanded;
     }
 
     async handleVoteClick(event) {
