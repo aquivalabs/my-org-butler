f92d641e4a2035f0822d7f6c76fc69a5f45ec8d4
Robert Sösemann <robert@soesemann.com>
2025-02-22 20:08:58 +0100

    Scratch org had no Copilot :-(

--- scripts/create-scratch-org.sh
+++ scripts/create-scratch-org.sh
@@ -28,9 +28,9 @@ sf data update record --sobject User --where "Name='User User'" --values "Langua
 echo "Running Apex Tests"
 execute sf apex run test --test-level RunLocalTests --wait 30 --code-coverage --result-format human
 
-# sf package install --package 04tHs000000W2H2 --publish-wait 5 --wait 10 --target-org $SCRATCH_ORG_ALIAS
+sf package install --package 04tHs000000W2H2 --publish-wait 5 --wait 10 --target-org $SCRATCH_ORG_ALIAS
 
-echo "Running SFX Scanner with Security, AppExchange and Coding Standards"
-sf code-analyzer run --rule-selector Security, AppExchange --output-file ./code-analyzer/output/code-analyzer-security.csv --output-file ./code-analyzer/output/code-analyzer-security.html
-sf code-analyzer run --rule-selector PMD --config-file ./code-analyzer-config.yml --output-file ./code-analyzer/code-analyzer-cleancode.csv --output-file ./code-analyzer/code-analyzer-cleancode.html
-sf scanner run dfa --output-file ./code-analyzer/output/graph-engine.csv --target ./ --projectdir ./ --category Security
+# echo "Running SFX Scanner with Security, AppExchange and Coding Standards"
+# sf code-analyzer run --rule-selector Security, AppExchange --output-file ./code-analyzer/output/code-analyzer-security.csv --output-file ./code-analyzer/output/code-analyzer-security.html
+# sf code-analyzer run --rule-selector PMD --config-file ./code-analyzer-config.yml --output-file ./code-analyzer/code-analyzer-cleancode.csv --output-file ./code-analyzer/code-analyzer-cleancode.html
+# sf scanner run dfa --output-file ./code-analyzer/output/graph-engine.csv --target ./ --projectdir ./ --category Security
