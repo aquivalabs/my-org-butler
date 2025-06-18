a8b2c05bf9210487b6b4fbfc6b050d1676600355
Robert Sösemann <robert@soesemann.com>
2025-06-18 14:35:28 +0200

    No LWC UI needed anymore. Get rid of UI stuff

--- force-app/main/default/lwc/typingChatMessage/typingChatMessage.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-import butlerLogo from '@salesforce/contentAssetUrl/myorgbutlertransparent_720';
-
-export default class TypingChatMessage extends LightningElement {
-    @api message;
-
-    butlerLogo = butlerLogo;
-}
